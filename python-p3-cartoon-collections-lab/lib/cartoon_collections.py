37ad00dbd20639a047e8cdc102696f0734a8b415
PhillipOdd <135106680+PhillipOdd@users.noreply.github.com>
2023-09-13 12:24:33 +0300

    Add files via upload

---
+++ python-p3-cartoon-collections-lab/lib/cartoon_collections.py
@@ -0,0 +1,17 @@
+def roll_call_dwarves():
+      for i, dwarf in enumerate(dwarves, start=1):
+        print(f"{i}. {dwarf}")
+
+def summon_captain_planet():
+    return [call.capitalize() + '!' for call in planeteer_calls]
+
+
+def long_planeteer_calls():
+    return any(len(call) > 4 for call in calls)
+
+def find_the_cheese(ingredients):
+    cheeses = ["cheddar", "gouda", "camembert"]
+    for ingredient in ingredients:
+        if ingredient in cheeses:
+            return ingredient
+    return None
